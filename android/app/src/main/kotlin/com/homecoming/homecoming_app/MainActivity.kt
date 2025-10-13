ff7cea71ca556282d59d17ec0991e30e5ce89a68
Sadeqalbaharna <sadeq.albaharna@gmail.com>
2025-10-13 23:49:18 +0300

    Enable true window transparency - see through to home screen!

--- android/app/src/main/kotlin/com/homecoming/homecoming_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/homecoming/homecoming_app/MainActivity.kt
@@ -1,5 +1,26 @@
 package com.homecoming.homecoming_app
 
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Make the window background transparent
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        
+        // Make status bar and navigation bar transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.TRANSPARENT
+        }
+    }
+}
