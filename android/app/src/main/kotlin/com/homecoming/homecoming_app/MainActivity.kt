c6fd6b5ada434369d20478534138f4272bd3b1a1
Sadeqalbaharna <sadeq.albaharna@gmail.com>
2025-10-14 11:52:55 +0300

    Enhanced transparency: Add decorView transparent background and system UI flags

--- android/app/src/main/kotlin/com/homecoming/homecoming_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/homecoming/homecoming_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.homecoming.homecoming_app
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.TransparencyMode
@@ -13,15 +14,31 @@ class MainActivity : FlutterActivity() {
         
         // Make the window background transparent
         window.setBackgroundDrawableResource(android.R.color.transparent)
+        
+        // Set window flags for transparency
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
         
+        // Make decorView transparent
+        window.decorView.setBackgroundColor(Color.TRANSPARENT)
+        
         // Make status bar and navigation bar transparent
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.statusBarColor = Color.TRANSPARENT
             window.navigationBarColor = Color.TRANSPARENT
+            
+            // Make system UI visible but transparent
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                window.setDecorFitsSystemWindows(false)
+            } else {
+                @Suppress("DEPRECATION")
+                window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+            }
         }
     }
     
