1d8397c33f58f8f5f7a0657fd7329f575768b7ee
Sadeqalbaharna <sadeq.albaharna@gmail.com>
2025-10-16 22:27:47 +0300

    fix: Update Android SDK versions for flutter_sound compatibility - compileSdk: 34 -> 35 - minSdk: 21 -> 24 - ndkVersion: 27.0.12077973 - targetSdk: 35 - Fixes Firebase Distribution build failure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 
 android {
     namespace = "com.homecoming.homecoming_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35  // Updated for flutter_sound and audioplayers_android
+    ndkVersion = "27.0.12077973"  // Updated for plugin compatibility
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,8 +25,8 @@ android {
         applicationId = "com.homecoming.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24  // Updated for flutter_sound requirement
+        targetSdk = 35  // Match compileSdk
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
